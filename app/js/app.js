a8222819ce12f28744b4ed156752cb5475351270
buggy <chandan7@icloud.com>
2014-11-07 09:10:01 -0800

    home_update

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,28 @@
-(function(){
+(function () {
     'use strict';
 
     angular
-        .module('registry',['firebase'])
-        .controller('mainController',mainController)
+        .module('registry', ['firebase'])
+        .controller('mainController', mainController)
 
-    function mainController($firebase){
+    function mainController($firebase) {
 
         var mc = this;
-
+        mc.isCompleted = false;
         var ref = new Firebase("https://buggy.firebaseio.com/");
         var sync = $firebase(ref);
 
         mc.data = sync.$asArray();
 
-        mc.addCustomer = function(firstName, lastName, tel, suitSyle) {
-            mc.data.$add({firstName: firstName, lastName: lastName, tel: tel, suitStyle: suitSyle});
-            mc.firstName = mc.lastName = mc.tel = mc.suitStyle = "";
+        mc.addCustomer = function (firstName, lastName, tel, suitSyle) {
+            mc.date = Date.now();
+            mc.data.$add({firstName: firstName, lastName: lastName, tel: tel, suitStyle: suitSyle, dateAdded: mc.date, isCompleted: mc.isCompleted});
+            mc.firstName = mc.lastName = mc.tel = mc.suitStyle = mc.date = "";
+        }
+
+        mc.toggleCompleted = function (customer) {
+            customer.isCompleted = !customer.isCompleted;
+            mc.data.$save(customer);
         }
 
     }
